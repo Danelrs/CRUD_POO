8a6ee720041ac3942dde88c8651c1ce6a390ca77
Danel Rolon <danelsalazarco@gmail.com>
2024-06-05 18:26:39 -0600

    ajuste

--- Database/catalogo_peliculas.sql
+++ Database/catalogo_peliculas.sql
@@ -16,7 +16,7 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `peliculas`
+-- Table structure for table `catalogo_peliculas`
 --
 
 DROP TABLE IF EXISTS `peliculas`;
